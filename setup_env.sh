ae23d81f9377f65f197b051e51e6a0c554fd1dfc
Kuba Zabielski <plajerowy@gmail.com>
2023-03-22 16:56:55 +0100

    commit

--- setup_env.sh
+++ setup_env.sh
@@ -12,6 +12,6 @@ pre-commit install
 deactivate
 
 # for plotly widget support
-pip install --user jupyterlab plotly==4.14.3 "ipywidgets>=7.5"
-jupyter labextension install jupyterlab-plotly@4.14.3
-jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.14.3
+#pip install --user jupyterlab plotly==4.14.3 "ipywidgets>=7.5"
+#jupyter labextension install jupyterlab-plotly@4.14.3
+#jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.14.3
